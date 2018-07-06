cf70099f05807a2ecfc9fff0e95a7c2afa575b9d
Nandang <nndg.ace3@gmail.com>
2018-07-06 10:28:50 +0700

    .

--- application/config/database.php
+++ application/config/database.php
@@ -4,12 +4,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-//$db['default']['hostname'] = '10.1.30.18';
-//$db['default']['username'] = 'db_itpu';
-//$db['default']['password'] = 'Uap)(*&^%';
+$db['default']['hostname'] = '10.1.30.18';
+$db['default']['username'] = 'db_itpu';
+$db['default']['password'] = 'Uap)(*&^%';
 $db['default']['database'] = 'db_academic';
 $db['default']['dbdriver'] = 'mysqli';// support with MYSQl,POSTGRE SQL, ORACLE,SQL SERVER
 $db['default']['dbprefix'] = '';
@@ -22,53 +19,6 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
-$db['adi'] = array(
-    'dsn'	=> '',
-    'hostname' => '10.1.10.230',
-    'username' => 'dbadmin',
-    'password' => '123456789',
-    'database' => 'siak4',
-    'dbdriver' => 'mysqli', // support with MYSQl,POSTGRE SQL, ORACLE,SQL SERVER
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
-
-$db['server'] = array(
-    'dsn'	=> '',
-//    'hostname' => 'localhost',
-//    'username' => 'root',
-//    'password' => '',
-    'hostname' => '10.1.30.88',
-    'username' => 'it',
-    'password' => 'itypap888',
-    'database' => 'siak4',
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
