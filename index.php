454bafc532cc464bc3a35b2ac02384ddcc5d7048
Nandang <nndg.ace3@gmail.com>
2018-07-06 16:49:08 +0700

    index again

--- index.php
+++ index.php
@@ -58,8 +58,8 @@
     define("serverRoot","http://pcam.podomorouniversity.ac.id", true);
     define("url_pas","http://pcam.podomorouniversity.ac.id/", true);
     define("url_pcam","http://pcam.podomorouniversity.ac.id/uath/__portal4SignIn", true);
-    define("url_students","http://studentpu.podomorouniversity.ac.id/home", true);
-    define("url_lecturers","http://lecturerpu.podomorouniversity.ac.id/home", true);
+    define("url_students","http://studentpu.podomorouniversity.ac.id/uath/__portal4SignIn", true);
+    define("url_lecturers","http://lecturerpu.podomorouniversity.ac.id/uath/__portal4SignIn", true);
     define("url_sign_out","http://portal.podomorouniversity.ac.id", true);
 
 
