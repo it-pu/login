0a4e7ecbf0f1e3d903f59e457721acbe6bdc8b57
alhadi rahman <alhadi.rahman@pu.local>
2018-10-24 09:08:02 +0700

    Define Server

--- application/config/database.php
+++ application/config/database.php
@@ -5,46 +5,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if($_SERVER['SERVER_NAME']!='localhost' && $_SERVER['SERVER_NAME'] == 'portal.podomorouniversity.ac.id') {
-
-    $db['default']['hostname'] = '10.1.30.18';
-    $db['default']['username'] = 'db_itpu';
-    $db['default']['password'] = 'Uap)(*&^%';
-    $db['default']['database'] = 'db_academic';
-    $db['default']['dbdriver'] = 'mysqli';// support with MYSQl,POSTGRE SQL, ORACLE,SQL SERVER
-    $db['default']['dbprefix'] = '';
-    $db['default']['pconnect'] = TRUE;
-    $db['default']['db_debug'] = FALSE;
-    $db['default']['cache_on'] = FALSE;
-    $db['default']['cachedir'] = '';
-    $db['default']['char_set'] = 'utf8';
-    $db['default']['dbcollat'] = 'utf8_general_ci';
-    $db['default']['swap_pre'] = '';
-    $db['default']['autoinit'] = TRUE;
-    $db['default']['stricton'] = FALSE;
-
-} else {
-    $db['default'] = array(
-        'dsn'	=> '',
-        'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'db_academic',
-        'dbdriver' => 'mysqli',
-        'dbprefix' => '',
-        'pconnect' => FALSE,
-        'db_debug' => (ENVIRONMENT !== 'production'),
-        'cache_on' => FALSE,
-        'cachedir' => '',
-        'char_set' => 'utf8',
-        'dbcollat' => 'utf8_general_ci',
-        'swap_pre' => '',
-        'encrypt' => FALSE,
-        'compress' => FALSE,
-        'stricton' => FALSE,
-        'failover' => array(),
-        'save_queries' => TRUE
-    );
+$ServerName = $_SERVER['SERVER_NAME'];
+switch ($ServerName) {
+    case 'localhost':
+        $db['default'] = array(
+            'dsn'   => '',
+            'hostname' => 'localhost',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'db_academic',
+            'dbdriver' => 'mysqli',
+            'dbprefix' => '',
+            'pconnect' => FALSE,
+            'db_debug' => (ENVIRONMENT !== 'production'),
+            'cache_on' => FALSE,
+            'cachedir' => '',
+            'char_set' => 'utf8',
+            'dbcollat' => 'utf8_general_ci',
+            'swap_pre' => '',
+            'encrypt' => FALSE,
+            'compress' => FALSE,
+            'stricton' => FALSE,
+            'failover' => array(),
+            'save_queries' => TRUE
+        );
+        break;
+    case 'portal.podomorouniversity.ac.id':
+        $db['default']['hostname'] = '10.1.30.18';
+        $db['default']['username'] = 'db_itpu';
+        $db['default']['password'] = 'Uap)(*&^%';
+        $db['default']['database'] = 'db_academic';
+        $db['default']['dbdriver'] = 'mysqli';// support with MYSQl,POSTGRE SQL, ORACLE,SQL SERVER
+        $db['default']['dbprefix'] = '';
+        $db['default']['pconnect'] = TRUE;
+        $db['default']['db_debug'] = FALSE;
+        $db['default']['cache_on'] = FALSE;
+        $db['default']['cachedir'] = '';
+        $db['default']['char_set'] = 'utf8';
+        $db['default']['dbcollat'] = 'utf8_general_ci';
+        $db['default']['swap_pre'] = '';
+        $db['default']['autoinit'] = TRUE;
+        $db['default']['stricton'] = FALSE;
+        break;
+    case 'demo.portal.podomorouniversity.ac.id':
+        $db['default']['hostname'] = 'localhost';
+        $db['default']['username'] = 'root';
+        $db['default']['password'] = 'Uap)(*&^%';
+        $db['default']['database'] = 'db_academic';
+        $db['default']['dbdriver'] = 'mysqli';// support with MYSQl,POSTGRE SQL, ORACLE,SQL SERVER
+        $db['default']['dbprefix'] = '';
+        $db['default']['pconnect'] = TRUE;
+        $db['default']['db_debug'] = FALSE;
+        $db['default']['cache_on'] = FALSE;
+        $db['default']['cachedir'] = '';
+        $db['default']['char_set'] = 'utf8';
+        $db['default']['dbcollat'] = 'utf8_general_ci';
+        $db['default']['swap_pre'] = '';
+        $db['default']['autoinit'] = TRUE;
+        $db['default']['stricton'] = FALSE;
+        break;    
+    default:
+        # code...
+        break;
 }
-
-
