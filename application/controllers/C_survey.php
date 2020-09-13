fd8a6838f6d4cd8e03219f6dffa77fe0895bc340
Nandang <nndg.ac3@gmail.com>
2020-09-13 11:17:22 +0700

    shared survey

--- application/controllers/C_survey.php
+++ application/controllers/C_survey.php
@@ -32,6 +32,143 @@ class C_survey extends MY_Controller {
         parent::template($content);
     }
 
+    public function checksurvey($Key){
+        // Cek apakah status survey publish
+        $dateNow = date('Y-m-d');
+        $dataSurvey = $this->db->query('SELECT * FROM db_it.surv_survey ss 
+                                        WHERE 
+                                        ss.Key = "'.$Key.'"
+                                        AND ss.Status = "1" 
+                                        AND ss.StartDate <= "'.$dateNow.'"
+                                        AND ss.EndDate >= "'.$dateNow.'"')
+            ->result_array();
+
+        $data['ValidSurvey'] = 0;
+        $data['Key'] = $Key;
+
+        if(count($dataSurvey)>0){
+            $data['ValidSurvey'] = 1;
+            $data['FormType'] = 'external';
+            $data['ID'] = $dataSurvey[0]['ID'];
+            $data['Title'] = $dataSurvey[0]['Title'];
+        }
+        $content = $this->load->view('page/survey/checksurvey',$data,true);
+        parent::template($content);
+
+
+    }
+
+    public function submitChecksurvey(){
+
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+
+        $token = $this->input->post('token');
+        $key = "s3Cr3T-G4N";
+        $data_arr = (array) $this->jwt->decode($token,$key);
+
+
+        if($data_arr['action']=='submitForm'){
+
+            $Username = '';
+            $UserType = ''.
+
+            $Status = 0;
+
+            if($data_arr['Participant']=='other'){
+                // Input form
+                $dataInput = array(
+                    'FullName' => $data_arr['FullName'],
+                    'Email' => $data_arr['Email'],
+                    'Phone' => $data_arr['Phone']
+                );
+
+                $this->db->insert('db_it.surv_external_user',$dataInput);
+
+                $UserType = 'other';
+                $Username = $this->db->insert_id();
+                $Status = 1;
+
+            } else {
+                $dataCk = $this->checkUser($data_arr['Username'],$data_arr['Password']);
+
+                if($dataCk['Status']==1 || $dataCk['Status']=='1'){
+                    $Status = 1;
+                    $UserType = $dataCk['UserType'];
+                    $Username = $data_arr['Username'];
+                }
+
+            }
+
+            if($Status==1 || $Status=='1'){
+                $dataSetSession = array(
+                    'portal_Username' => $Username,
+                    'portal_SurveyID' => $data_arr['SurveyID'],
+                    'portal_FormType' => 'external',
+                    'portal_UserType' => $UserType,
+                    'portal_Key' => $data_arr['Key'],
+                    'portal_SurveyLogin' => true
+                );
+
+                $this->session->set_userdata($dataSetSession);
+            } else {
+                $this->session->sess_destroy();
+            }
+
+            return print_r(json_encode(array('Status'=>$Status)));
+
+        }
+
+    }
+
+    function checkUser($Username,$Password){
+        $Pass = $this->genratePassword($Username,$Password);
+        $checkEmp = $this->db->select('Name')->get_where('db_employees.employees',
+            array(
+                'NIP' => $Username,
+                'Password' => $Pass
+            ))->result_array();
+
+
+        $result = array(
+            'Status' => 0
+        );
+
+        if(count($checkEmp)<=0){
+            $checkStd = $this->db->select('Name')->get_where('db_academic.auth_students',
+                array(
+                    'NPM' => $Username,
+                    'Password' => $Pass
+                ))->result_array();
+
+            if(count($checkStd)>0){
+                $result = array(
+                    'Name' => $checkStd[0]['Name'],
+                    'Status' => 1,
+                    'UserType' => 'std'
+                );
+            }
+        } else {
+            $result = array(
+                'Name' => $checkEmp[0]['Name'],
+                'Status' => 1,
+                'UserType' => 'emp'
+            );
+        }
+
+        return $result;
+
+    }
+
+    private function genratePassword($Username,$Password){
+
+        $plan_password = $Username.''.$Password;
+        $pas = md5($plan_password);
+        $pass = sha1('jksdhf832746aiH{}{()&(*&(*'.$pas.'HdfevgyDDw{}{}{;;*766&*&*');
+
+        return $pass;
+    }
+
     public function surveyStart(){
 
         header('Access-Control-Allow-Origin: *');
@@ -69,6 +206,7 @@ class C_survey extends MY_Controller {
             'portal_Username' => $data_arr['Username'],
             'portal_SurveyID' => $data_arr['SurveyID'],
             'portal_UserType' => $data_arr['UserType'],
+            'portal_FormType' => 'internal',
             'portal_SurveyLogin' => true
         );
 
